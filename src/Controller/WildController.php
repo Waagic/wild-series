6adcb56e638b41eeff6ce8e937c9bc1739a1c8ee
Lucas Marguiron <lucas.marguiron@gmail.com>
2020-05-20 12:58:16 +0200

    Quete 5

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -11,10 +11,22 @@ class WildController extends AbstractController
     /**
      * @Route("/wild", name="wild_index")
      */
-    public function index() :Response
+    public function index(): Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+
+    /**
+     * @Route("/wild/show/{slug}", requirements={"slug"="[a-z0-9-]+"}, name="wild_show")
+     */
+    public function show(string $slug = "Aucune série sélectionnée, veuillez choisir une série"): Response
+    {
+        $slug = ucfirst(str_replace("-", " ", $slug));
+        return $this->render('wild/show.html.twig', [
+            'slug' => $slug,
+        ]);
+
     }
 }
