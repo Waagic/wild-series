64620c7eda473a29d2d6e5e122d2be416257881a
Lucas Marguiron <lucas.marguiron@gmail.com>
2020-06-04 11:38:42 +0200

    Quete 13

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Quête 10 : https://www.loom.com/share/10f158fd3bff44cba39217a95af2adff
 Quête 11 : https://www.loom.com/share/65604638e3fe47ef89dfdaef328470db
 
 Quête 12 : https://www.loom.com/share/67bf4c56dc6e46bb98d32b5a2524c76e
+
+Quête 13 : https://www.loom.com/share/89bfe6fe4d4b4c07adb83347c224851b
--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -6,9 +6,16 @@ use App\Repository\ProgramRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ProgramRepository::class)
+ * @UniqueEntity(
+ *     "title",
+ *     errorPath="title",
+ *     message="Ce titre existe déjà"
+ * )
  */
 class Program
 {
@@ -21,11 +28,19 @@ class Program
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Length(max="255")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank()
+     * @Assert\Regex(
+     *     pattern="/([plus belle la vie])\w+/i",
+     *     match=false,
+     *     message="Ici on parle de vraies séries"
+     * )
      */
     private $summary;
 
