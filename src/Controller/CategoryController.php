10ddb252129293136c14398803c474d14c22884d
Lucas Marguiron <lucas.marguiron@gmail.com>
2020-06-09 14:45:37 +0200

    Cours quete 20

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Form\CategoryType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,7 +19,9 @@ class CategoryController extends AbstractController
 {
     /**
      * @Route("/add", name="add")
+     * @param Request $request
      * @return Response A response instance
+     * @IsGranted("ROLE_ADMIN")
      */
     public function add(Request $request):Response
     {
